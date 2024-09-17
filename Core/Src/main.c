37cf9fc6639a91e19b4cc9f58eb3c721dd965fe9
Gabriel Erick <g197301@dac.unicamp.br>
2024-09-17 00:19:27 -0300

    incluindo no IOC o DMA settings do I2C

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "dma.h"
 #include "i2c.h"
 #include "usart.h"
 #include "tim.h"
@@ -93,6 +94,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_LPUART1_UART_Init();
   MX_TIM1_Init();
   MX_TIM16_Init();
